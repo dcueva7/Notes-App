a078c8de904fe3de63711a1082305b9a9e794224
Daniel Cueva <dcueva@usc.edu>
2023-04-27 16:14:51 -0700

    added functionality for updating note and arrow funciton

--- ui/src/NotePage.js
+++ ui/src/NotePage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { useState, useEffect} from 'react'
 
 import { Card, CardHeader, CardBody, Heading, Textarea, IconButton } from '@chakra-ui/react'
@@ -9,7 +9,7 @@ import { ArrowBackIcon } from '@chakra-ui/icons'
 
 export const NotePage = (props ) => {
 
-    
+    const nav = useNavigate();
     const params = useParams();
 
     const [ note, setNote ] = useState("")
@@ -21,14 +21,33 @@ export const NotePage = (props ) => {
         
         }, [params.id]
     )
-    
+
+    const updateNote = () => {
+      fetch(`/api/update/${params.id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type' : 'application/json'
+        },
+        body : JSON.stringify({'body' : note.body})
+        
+      })
+        .then(response => response.json())
+        .then(json => console.log(json))
+
+      nav('/');
+
+    }
 
     return (
       <Flex h="100vh" align="center" justify="center">
         <Card sx={{ width: '60%', textAlign: 'center', minHeight: '400px' }}>
             <CardHeader textAlign='center'>
               <Flex justifyContent="space-between" alignItems="center">
-                <IconButton icon={<ArrowBackIcon />} aria-label="Back" />
+                <IconButton 
+                icon={<ArrowBackIcon />} 
+                aria-label="Back" 
+                onClick={updateNote} />
+
                 <Heading size='lg'>Note</Heading>
                 <div style={{ width: '24px' }}></div>
               </Flex>
@@ -41,7 +60,6 @@ export const NotePage = (props ) => {
                     maxHeight="200px"
                     sx={{ width: '100%', maxWidth: 'none', whiteSpace: 'pre-wrap' }} />
             </CardBody>
-            
         </Card>
     </Flex>
         
